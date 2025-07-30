aa80e2f4bf5b5255131148d504cfd6e6ea7f303f
tehballade <dhdy9a1@gmail.com>
2025-07-30 09:25:27 +0700

    Add files via upload

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt-get remove -y docker.io docker-doc docker-compose \
+  docker-compose-v2 podman-docker containerd runc
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io \
+  docker-buildx-plugin docker-compose-plugin
+
+echo "✅ Cài xong Docker và Docker Compose!"
+docker --version
+docker compose version
